a154bdacf2d23eeec5c737ce3c9a60b9d312873a
david <dgrenner@gmx.de>
2022-05-30 14:51:04 +0200

    Add Ros2 subscribor for first basic test

--- garbage_detection/garbage_detection/gargarbage_detection_node.py
+++ garbage_detection/garbage_detection/gargarbage_detection_node.py
@@ -4,34 +4,30 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class MinimalPublisher(Node):
+class GarbageDetection(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('minimal_subscriber')
+        
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.imageCallback,
+            10)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.subscription  # prevent unused variable warning
+
+    def imageCallback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    node = GarbageDetection()
+    rclpy.spin(node)
 
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
