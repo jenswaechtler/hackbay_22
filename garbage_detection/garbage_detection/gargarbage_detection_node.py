6723d6429729d8fbbf4f7cf7e497fbd48fd1f265
david <dgrenner@gmx.de>
2022-05-30 15:09:13 +0200

    Add tasks

--- garbage_detection/garbage_detection/gargarbage_detection_node.py
+++ garbage_detection/garbage_detection/gargarbage_detection_node.py
@@ -1,17 +1,17 @@
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
 
 
 class GarbageDetection(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('garbage_detection_node')
         
+        # TODO: change to msg_image and change topic
         self.subscription = self.create_subscription(
             String,
-            'topic',
+            'image',
             self.imageCallback,
             10)
 
@@ -19,6 +19,13 @@ class GarbageDetection(Node):
 
     def imageCallback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
+        
+        # Do all the magic here
+        #
+        # 1. Ros image to openCV image
+        # 2. Garbage detection (open CV stuff -> bounding box)
+        # 3. Publish video as youtube stream
+        #
 
 
 def main(args=None):
