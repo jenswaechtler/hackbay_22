a46a01b5995a6c6ba50901d69fb0f72d870516e3
Jens Waechtler <13545730+jenswaechtler@users.noreply.github.com>
2022-05-30 15:51:37 +0200

    Add image to cv2

--- garbage_detection/garbage_detection/gargarbage_detection_node.py
+++ garbage_detection/garbage_detection/gargarbage_detection_node.py
@@ -3,6 +3,9 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
 
 class GarbageDetection(Node):
 
@@ -20,15 +23,23 @@ class GarbageDetection(Node):
 
     def imageCallback(self, msg):
         # self.get_logger().info('I heard: "%s"' % msg.data)
-         self.get_logger().info('Image received ')
+        self.get_logger().info('Image received ')
         
         # Do all the magic here
         #
         # 1. Ros image to openCV image
+        try:
+            cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+            show_image(cv_image)
+        except CvBridgeError, e:
+            rospy.logerr("CvBridge Error: {0}".format(e))
         # 2. Garbage detection (open CV stuff -> bounding box)
         # 3. Publish video as youtube stream
         #
 
+ def show_image(img):
+     cv2.imshow("Image Window", img)
+     cv2.waitKey(3)
 
 def main(args=None):
     rclpy.init(args=args)
