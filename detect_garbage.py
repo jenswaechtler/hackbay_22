4f22882f0e45a37db3716aef08967f52dcc356e4
Zacharias Zeiler <zacharias.zeiler@outlook.com>
2022-05-30 20:20:06 +0200

    Add bounding boxes

--- detect_garbage.py
+++ detect_garbage.py
@@ -16,6 +16,15 @@ while True:
 	
 	blurred = cv2.medianBlur(color_filtered, 9)
 
+	imGray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(imGray, 127, 255, 0)
+
+	contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+	for contour in contours:
+		x,y,w,h = cv2.boundingRect(contour)
+		cv2.rectangle(blurred, (x,y), (x+w, y+h), (0,0,255), 2)
+		
 
 	cv2.imshow('frame', frame)
 	cv2.imshow('Output', blurred)
